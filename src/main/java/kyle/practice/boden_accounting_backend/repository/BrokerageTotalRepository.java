27ec8e50435664e2f2140d52db4f92f315a437de
kyleboden <bodenkyle8@gmail.com>
2026-05-11 21:04:43 -0600

    removing totalBrokerage Table

--- src/main/java/kyle/practice/boden_accounting_backend/repository/BrokerageTotalRepository.java
+++
@@ -1,7 +0,0 @@
-package kyle.practice.boden_accounting_backend.repository;
-
-import kyle.practice.boden_accounting_backend.entity.BrokerageTotal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrokerageTotalRepository extends JpaRepository<BrokerageTotal, Long> {
-}
